f3e2eac2b63dbe50841e6ca4877d8fdc92ccc61a
Лев <helmo.designer1@gmail.com>
2017-10-18 22:55:19 +0300

    13.1 e added

--- FALL2017/MISC/Determinant/detN/Program.cs
+++ FALL2017/MISC/Determinant/detN/Program.cs
@@ -6,15 +6,17 @@ namespace detN
 	{
 		public static void Main()
 		{
-			double[,] m =
+			double[,] matrix =
 			{
-				{3, 2, 4, 5 },
-				{4, -3, 2, -4 },
-				{5, -2, -3, -7 },
-				{-3, 4, 2, 9 }
+				{-5, -7, -2, 2, -2, 16 },
+				{0, 0, 4, 0, -5, 0 },
+				{2, 0, -2, 0, 2, 0},
+				{6, 4, 6, -1, 15, -5},
+				{5, -4, 10, 1, 14, 6},
+				{3, 0, -2, 0, 3, 0}
 			};
 
-			double det = Calculate(m);
+			double det = Calculate(matrix);
 			Console.WriteLine(det);
 		}
 
@@ -25,9 +27,9 @@ namespace detN
 			
 			return -1;
 		}
-		public static double[,] CreateMinor(double[,] m, int i, int j)
+		public static double[,] CreateMinor(double[,] matrix, int i, int j)
 		{
-			int order = m.GetLength(0);
+			int order = matrix.GetLength(0);
 			double[,] minor = new double[order - 1, order - 1];
 			int x = 0, y;
 			for (int l = 0; l < order; l++, x++)
@@ -38,7 +40,7 @@ namespace detN
 					for (int n = 0; n < order; n++)
 						if (n != j)
 						{
-							minor[x, y] = m[l, n];
+							minor[x, y] = matrix[l, n];
 							y++;
 						}
 				}
@@ -48,24 +50,24 @@ namespace detN
 			return minor;
 		}
 
-		public static double Calculate(double[,] m)
+		public static double Calculate(double[,] matrix)
 		{
-			int order = m.GetLength(0);
+			int order = matrix.GetLength(0);
 			if (order == 2)
 			{
-				return m[0, 0] * m[1, 1] - m[1, 0] * m[0, 1];
+				return matrix[0, 0] * matrix[1, 1] - matrix[1, 0] * matrix[0, 1];
 			}
 			if (order > 2)
 			{
 				double result = 0;
 				for (int j = 0; j < order; j++)
 				{
-					double[,] tmp = CreateMinor(m, 0, j);
-					result += m[0, j] * (Sign(0, j) * Calculate(tmp));
+					double[,] tmp = CreateMinor(matrix, 0, j);
+					result += matrix[0, j] * (Sign(0, j) * Calculate(tmp));
 				}
 				return result;
 			}
-			return m[0, 0];
+			return matrix[0, 0];
 		}
 	}
 }
