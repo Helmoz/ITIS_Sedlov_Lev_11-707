06d84f2e743b84ab94fff47256638b205879d1a0
Лев <helmo.designer1@gmail.com>
2017-11-19 22:36:32 +0300

    add 3_5

---
+++ FALL2017/PS/PS2/part3_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+	// 3_5
+	// Произвести вычисления по рекуррентной схеме
+	// Седлов Лев 11-707
+
+	static double GetCos(double eps)
+	{
+		
+		double previousNumber = 5;
+		double currentNumber = 0;
+		
+		while (Math.Abs(currentNumber - previousNumber) >= eps)
+		{
+			previousNumber = currentNumber;
+			currentNumber = Math.Cos(previousNumber);
+		}
+		return currentNumber;
+	}
+	static void Main()
+	{
+		double eps = 0.00000000000001;
+		double result = GetCos(eps);
+
+		Console.WriteLine($"Результат вычислений: {result} {Math.Acos(result)}");
+	}
+}
