48feec0520349259e7676ca3e6b5b46e09b6e7ec
Лев <helmo.designer1@gmail.com>
2017-12-04 21:03:15 +0300

    Final code review

--- FALL2017/PS/PS3/BigInteger/Program.cs
+++ FALL2017/PS/PS3/BigInteger/Program.cs
@@ -8,16 +8,16 @@ namespace BigInteger
 	// Напишите программу перевода многозначного числа (с количеством знаков больше 20)
 	// в системы счисления с основанием два, восемь, шестнадцать.
 	// Седлов Лев 11-707
-	
+
 	public class BigInteger
-    {
+	{
 		private bool _sign;
 
-        private readonly List<byte> _digits =  new List<byte>();
-		
-        #region Constructors
+		private readonly List<byte> _digits = new List<byte>();
 
-        public BigInteger(string s)
+		#region Constructors
+
+		public BigInteger(string s)
 		{
 			var output = new BigInteger(new List<byte> { 0 });
 
@@ -30,13 +30,13 @@ namespace BigInteger
 				check = true;
 				add++;
 			}
-			
+
 			for (int i = add; i < s.Length; i++)
 			{
 				output *= new BigInteger(new List<byte> { 10 });
 				output += new BigInteger(new List<byte> { (byte)(s[i] - '0') });
 			}
-			
+
 			_digits = output._digits;
 
 			_sign = check;
@@ -57,173 +57,173 @@ namespace BigInteger
 		{
 		}
 
-        #endregion
+		#endregion
 
-        #region Addition
+		#region Addition
 
-        public static BigInteger Addition(BigInteger first, BigInteger second)
-        {
+		public static BigInteger Addition(BigInteger first, BigInteger second)
+		{
 
-            var sum = new BigInteger(first._digits);
-            var isCarry = false;
-            for (int i = 0; i < second._digits.Count; i++)
-            {
-                var add = sum._digits[i] + second._digits[i] + (isCarry ? 1 : 0);
-                isCarry = (add >> 8) > 0;
-                sum._digits[i] = (byte)add;
-            }
-            var carryIndex = second._digits.Count;
-            while (isCarry)
-            {
-                if (sum._digits.Count > carryIndex)
-                {
-                    sum._digits[carryIndex] += 1;
-                    if (sum._digits[carryIndex] != 0) isCarry = false;
-                    carryIndex++;
-                }
-                else
-                {
-                    sum._digits.Add(1);
-                    isCarry = false;
-                }
-            }
-            return sum;
-        }
-        public static BigInteger operator +(BigInteger first, BigInteger second)
-        {
-            if (AbsCompare(first, second) == -1)
-            {
-                var temp = second;
-                second = first;
-                first = temp;
-            }
-            var result = first._sign == second._sign ? Addition(first, second) : Substraction(first, second);
-            result._sign = first._sign;
-            return result;
-        }
-
-        #endregion
-
-        #region Substraction
-
-        public static BigInteger Substraction(BigInteger first, BigInteger second)
-        {
-            var sum = new BigInteger(first._digits);
-            var isCarry = false;
-            for (int i = 0; i < second._digits.Count; i++)
-            {
-                var isCarryNext = sum._digits[i] < second._digits[i];
-                sum._digits[i] -= (byte)(second._digits[i] + (isCarry ? 1 : 0));
-                isCarry = isCarryNext;
-            }
-            var carryIndex = second._digits.Count;
-            while (isCarry)
-            {
-                if (sum._digits[carryIndex] != 0)
+			var sum = new BigInteger(first._digits);
+			var isCarry = false;
+			for (int i = 0; i < second._digits.Count; i++)
+			{
+				var add = sum._digits[i] + second._digits[i] + (isCarry ? 1 : 0);
+				isCarry = (add >> 8) > 0;
+				sum._digits[i] = (byte)add;
+			}
+			var carryIndex = second._digits.Count;
+			while (isCarry)
+			{
+				if (sum._digits.Count > carryIndex)
+				{
+					sum._digits[carryIndex] += 1;
+					if (sum._digits[carryIndex] != 0) isCarry = false;
+					carryIndex++;
+				}
+				else
+				{
+					sum._digits.Add(1);
 					isCarry = false;
-                sum._digits[carryIndex] -= 1;
-                carryIndex++;
-            }
-            var lastIndex = sum._digits.Count;
-			while (lastIndex != 0 && sum._digits[--lastIndex] == 0){}
-            sum._digits.RemoveRange(lastIndex + 1, sum._digits.Count - lastIndex - 1);
+				}
+			}
+			return sum;
+		}
+		public static BigInteger operator +(BigInteger first, BigInteger second)
+		{
+			if (AbsCompare(first, second) == -1)
+			{
+				var temp = second;
+				second = first;
+				first = temp;
+			}
+			var result = first._sign == second._sign ? Addition(first, second) : Substraction(first, second);
+			result._sign = first._sign;
+			return result;
+		}
 
-            return sum;
-        }
+		#endregion
 
-        public static BigInteger operator -(BigInteger first, BigInteger second)
-        {
-            if (first.Equals(second))
-                second = new BigInteger(second._digits) { _sign = !first._sign };
-            else
-                second._sign = !second._sign;
+		#region Substraction
 
-            var result = first + second;
+		public static BigInteger Substraction(BigInteger first, BigInteger second)
+		{
+			var sum = new BigInteger(first._digits);
+			var isCarry = false;
+			for (int i = 0; i < second._digits.Count; i++)
+			{
+				var isCarryNext = sum._digits[i] < second._digits[i];
+				sum._digits[i] -= (byte)(second._digits[i] + (isCarry ? 1 : 0));
+				isCarry = isCarryNext;
+			}
+			var carryIndex = second._digits.Count;
+			while (isCarry)
+			{
+				if (sum._digits[carryIndex] != 0)
+					isCarry = false;
+				sum._digits[carryIndex] -= 1;
+				carryIndex++;
+			}
+			var lastIndex = sum._digits.Count;
+			while (lastIndex != 0 && sum._digits[--lastIndex] == 0) { }
+			sum._digits.RemoveRange(lastIndex + 1, sum._digits.Count - lastIndex - 1);
 
-            second._sign = !second._sign;
+			return sum;
+		}
 
-            return result;
-        }
+		public static BigInteger operator -(BigInteger first, BigInteger second)
+		{
+			if (first.Equals(second))
+				second = new BigInteger(second._digits) { _sign = !first._sign };
+			else
+				second._sign = !second._sign;
 
-        #endregion
+			var result = first + second;
 
-        #region Multiplication
+			second._sign = !second._sign;
 
-        public static BigInteger operator *(BigInteger number, byte b)
-        {
-            var result = new BigInteger(number._digits);
+			return result;
+		}
 
-            byte carry = 0;
+		#endregion
 
-            for (int i = 0; i < result._digits.Count; i++)
-            {
-                int intermediate = result._digits[i] * b;
-                var addCarry = ((intermediate & 255) + carry) >> 8;
-                result._digits[i] = (byte)((intermediate & 255) + carry);
-                carry = (byte)((intermediate >> 8) + addCarry);
-            }
+		#region Multiplication
 
-            if (carry > 0)
-                result._digits.Add(carry);
+		public static BigInteger operator *(BigInteger number, byte b)
+		{
+			var result = new BigInteger(number._digits);
 
-            return result;
-        }
+			byte carry = 0;
 
-        public static BigInteger operator *(BigInteger first, BigInteger second)
-        {
-            var result = new BigInteger();
-            if (AbsCompare(first, second) == -1)
-            {
-                var temp = second;
-                second = first;
-                first = temp;
-            }
+			for (int i = 0; i < result._digits.Count; i++)
+			{
+				int intermediate = result._digits[i] * b;
+				var addCarry = ((intermediate & 255) + carry) >> 8;
+				result._digits[i] = (byte)((intermediate & 255) + carry);
+				carry = (byte)((intermediate >> 8) + addCarry);
+			}
 
-            for (int i = 0; i < second._digits.Count; i++)
-                MultiplicationAdd(result, first * second._digits[i], i);
-            var lastIndex = result._digits.Count;
-            while (lastIndex != 0 && result._digits[--lastIndex] == 0){}
-            result._digits.RemoveRange(lastIndex + 1, result._digits.Count - lastIndex - 1);
-            result._sign = first._sign != second._sign;
-            return result;
-        }
+			if (carry > 0)
+				result._digits.Add(carry);
 
-        private static void MultiplicationAdd(BigInteger result, BigInteger longNumber, int index)
-        {
-            var isCarry = false;
-            for (int i = 0; i < longNumber._digits.Count; i++)
-            {
-                if (result._digits.Count == i + index)
+			return result;
+		}
+
+		public static BigInteger operator *(BigInteger first, BigInteger second)
+		{
+			var result = new BigInteger();
+			if (AbsCompare(first, second) == -1)
+			{
+				var temp = second;
+				second = first;
+				first = temp;
+			}
+
+			for (int i = 0; i < second._digits.Count; i++)
+				MultiplicationAdd(result, first * second._digits[i], i);
+			var lastIndex = result._digits.Count;
+			while (lastIndex != 0 && result._digits[--lastIndex] == 0) { }
+			result._digits.RemoveRange(lastIndex + 1, result._digits.Count - lastIndex - 1);
+			result._sign = first._sign != second._sign;
+			return result;
+		}
+
+		private static void MultiplicationAdd(BigInteger result, BigInteger longNumber, int index)
+		{
+			var isCarry = false;
+			for (int i = 0; i < longNumber._digits.Count; i++)
+			{
+				if (result._digits.Count == i + index)
 					result._digits.Add(0);
-                var add = result._digits[i + index] + longNumber._digits[i] + (isCarry ? 1 : 0);
+				var add = result._digits[i + index] + longNumber._digits[i] + (isCarry ? 1 : 0);
 
-                isCarry = (add >> 8) > 0;
-                result._digits[i + index] = (byte)add;
+				isCarry = (add >> 8) > 0;
+				result._digits[i + index] = (byte)add;
 
-            }
-            var carryIndex = longNumber._digits.Count + index;
-            while (isCarry)
-            {
-                if (result._digits.Count > carryIndex)
-                {
-                    result._digits[carryIndex] += 1;
-                    if (result._digits[carryIndex] != 0)
+			}
+			var carryIndex = longNumber._digits.Count + index;
+			while (isCarry)
+			{
+				if (result._digits.Count > carryIndex)
+				{
+					result._digits[carryIndex] += 1;
+					if (result._digits[carryIndex] != 0)
 						isCarry = false;
-                    carryIndex++;
-                }
-                else
-                {
-                    result._digits.Add(1);
-                    isCarry = false;
-                }
-            }
-        }
+					carryIndex++;
+				}
+				else
+				{
+					result._digits.Add(1);
+					isCarry = false;
+				}
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Division
+		#region Division
 
-        public static Tuple<BigInteger, BigInteger> Divide(BigInteger dividend, BigInteger divider)
+		public static Tuple<BigInteger, BigInteger> Divide(BigInteger dividend, BigInteger divider)
 		{
 			var result = new BigInteger();
 			var residue = new BigInteger(dividend._digits, dividend._sign);
@@ -262,7 +262,7 @@ namespace BigInteger
 
 
 			var lastIndex = result._digits.Count;
-			while (lastIndex != 0 && result._digits[--lastIndex] == 0){}
+			while (lastIndex != 0 && result._digits[--lastIndex] == 0) { }
 			result._digits.RemoveRange(lastIndex + 1, result._digits.Count - lastIndex - 1);
 			result._sign = dividend._sign != divider._sign;
 			return new Tuple<BigInteger, BigInteger>(result, residue);
@@ -291,7 +291,7 @@ namespace BigInteger
 
 			return new Tuple<Byte, BigInteger>((byte)down, residueOut);
 		}
-        
+
 		public static BigInteger operator /(BigInteger first, BigInteger second)
 		{
 			return Divide(first, second).Item1;
@@ -302,11 +302,11 @@ namespace BigInteger
 		}
 
 
-        #endregion
+		#endregion
 
-        #region Comparison
+		#region Comparison
 
-        public static bool operator >(BigInteger first, BigInteger second)
+		public static bool operator >(BigInteger first, BigInteger second)
 		{
 			if (first._sign)
 			{
@@ -335,150 +335,150 @@ namespace BigInteger
 		{
 			return (first == second) || (first > second);
 		}
-		
-        public static int AbsCompare(BigInteger first, BigInteger second)
-        {
-            if (first._digits.Count < second._digits.Count)
-                return -1;
 
-            if (first._digits.Count > second._digits.Count)
-                return 1;
+		public static int AbsCompare(BigInteger first, BigInteger second)
+		{
+			if (first._digits.Count < second._digits.Count)
+				return -1;
 
-            for (int i = first._digits.Count - 1; i >= 0; i--)
-            {
-                if (first._digits[i] < second._digits[i])
-                    return -1;
+			if (first._digits.Count > second._digits.Count)
+				return 1;
 
-                if (first._digits[i] > second._digits[i])
-                    return 1;
-            }
-            return 0;
-        }
+			for (int i = first._digits.Count - 1; i >= 0; i--)
+			{
+				if (first._digits[i] < second._digits[i])
+					return -1;
+
+				if (first._digits[i] > second._digits[i])
+					return 1;
+			}
+			return 0;
+		}
 
 		#endregion
 
 		#region Converters
 
 		public override string ToString()
-        {
-            var sb = new StringBuilder();
-            var add = 0;
-            if (_sign)
-            {
-                sb.Append('-');
-                add++;
-            }
-            int counter = 0;
-            var forOut = new BigInteger(_digits);
-            while (forOut._digits.Count > 1 || forOut._digits[0] > 9)
-            {
-                var div = forOut / 10;
-                var mod = forOut % 10;
-                forOut = div;
-                sb.Insert(add, (char)('0' + mod._digits[0]));
-                counter++;
-                if (counter == 3)
-                {
-                    sb.Insert(0, " ");
-                    counter = 0;
-                }
-            }
-            sb.Insert(add, (char)('0' + forOut._digits[0]));
-            return sb.ToString().TrimStart(' ');
-        }
+		{
+			var sb = new StringBuilder();
+			var add = 0;
+			if (_sign)
+			{
+				sb.Append('-');
+				add++;
+			}
+			int counter = 0;
+			var forOut = new BigInteger(_digits);
+			while (forOut._digits.Count > 1 || forOut._digits[0] > 9)
+			{
+				var div = forOut / 10;
+				var mod = forOut % 10;
+				forOut = div;
+				sb.Insert(add, (char)('0' + mod._digits[0]));
+				counter++;
+				if (counter == 3)
+				{
+					sb.Insert(0, " ");
+					counter = 0;
+				}
+			}
+			sb.Insert(add, (char)('0' + forOut._digits[0]));
+			return sb.ToString().TrimStart(' ');
+		}
 
-        public static string ConverToBinary(BigInteger number)
-        {
+		public static string ConverToBinary(BigInteger number)
+		{
 			var counter = 0;
-            var sb = new StringBuilder();
-            while (number > 0)
-            {
+			var sb = new StringBuilder();
+			while (number > 0)
+			{
 				sb.Insert(0, number % 2);
 				number = number / 2;
-                counter++;
-                if (counter == 4)
-                {
-                    sb.Insert(0, " ");
-                    counter = 0;
-                }
-            }
-            for (int i = 0; i < 4 - counter; i++)
-                sb.Insert(0, "0");
-            return sb.ToString().TrimStart(' ');
-        }
+				counter++;
+				if (counter == 4)
+				{
+					sb.Insert(0, " ");
+					counter = 0;
+				}
+			}
+			for (int i = 0; i < 4 - counter; i++)
+				sb.Insert(0, "0");
+			return sb.ToString().TrimStart(' ');
+		}
 
-        public static string ConverToOct(BigInteger number)
-        {
-            var counter = 0;
-            var sb = new StringBuilder();
-            while (number > 0)
-            {
-				sb.Insert(0, number % 8); 
-                number = number / 8;
-                counter++;
-                if (counter == 3)
-                {
-                    sb.Insert(0, " ");
-                    counter = 0;
-                }
-            }
-            return sb.ToString().TrimStart(' ');
-        }
-
-        public static string ConverToHex(BigInteger number)
-        {
+		public static string ConverToOct(BigInteger number)
+		{
 			var counter = 0;
-            var sb = new StringBuilder();
-            while (number > 0)
-            {
-                var residue = number % 16;
-                number = number / 16;
-                if (residue >= 10)
-                    sb.Insert(0, (char)(residue._digits[0] + 55));
-                else
-                    sb.Insert(0, residue);
-                counter++;
-                if (counter == 4)
-                {
-                    sb.Insert(0, " ");
-                    counter = 0;
-                }
-            }
-            return sb.ToString().TrimStart(' ');
-        }
+			var sb = new StringBuilder();
+			while (number > 0)
+			{
+				sb.Insert(0, number % 8);
+				number = number / 8;
+				counter++;
+				if (counter == 3)
+				{
+					sb.Insert(0, " ");
+					counter = 0;
+				}
+			}
+			return sb.ToString().TrimStart(' ');
+		}
+
+		public static string ConverToHex(BigInteger number)
+		{
+			var counter = 0;
+			var sb = new StringBuilder();
+			while (number > 0)
+			{
+				var residue = number % 16;
+				number = number / 16;
+				if (residue >= 10)
+					sb.Insert(0, (char)(residue._digits[0] + 55));
+				else
+					sb.Insert(0, residue);
+				counter++;
+				if (counter == 4)
+				{
+					sb.Insert(0, " ");
+					counter = 0;
+				}
+			}
+			return sb.ToString().TrimStart(' ');
+		}
 
 		#endregion
 
 		#region Implicit operators
 
 		public static implicit operator BigInteger(int value)
-	    {
-		    return new BigInteger(value.ToString());
-	    }
-	    public static implicit operator BigInteger(uint value)
-	    {
-		    return new BigInteger(value.ToString());
-	    }
+		{
+			return new BigInteger(value.ToString());
+		}
+		public static implicit operator BigInteger(uint value)
+		{
+			return new BigInteger(value.ToString());
+		}
 
-	    public static implicit operator BigInteger(long value)
-	    {
-		    return new BigInteger(value.ToString());
-	    }
-	    public static implicit operator BigInteger(ulong value)
-	    {
-		    return new BigInteger(value.ToString());
-	    }
+		public static implicit operator BigInteger(long value)
+		{
+			return new BigInteger(value.ToString());
+		}
+		public static implicit operator BigInteger(ulong value)
+		{
+			return new BigInteger(value.ToString());
+		}
 
-	    public static implicit operator BigInteger(string value)
-	    {
-		    return new BigInteger(value);
-	    }
+		public static implicit operator BigInteger(string value)
+		{
+			return new BigInteger(value);
+		}
 
-		#endregion 
-    }
+		#endregion
+	}
 
 	class Program
-	{		
+	{
 		static void Main()
 		{
 			BigInteger number = "12321321357236763462623463247246238463242649247629472649723642937462397462349723642937462934";
@@ -488,5 +488,5 @@ namespace BigInteger
 			Console.WriteLine(BigInteger.ConverToHex(number));
 		}
 
-    }
+	}
 }
